0b54d28872edee4a9495d069a9cac7a1aa713c03
bouget.thomas@gmail.com <bouget.thomas@gmail.com@f30f5b82-06ff-881c-f7a4-04f683a4d23f>
2012-02-11 13:44:04 +0000

    page d'accueil

--- DwarfLauncher/src/mmm/dwarf/launcher/DwarfLauncherActivity.java
+++ DwarfLauncher/src/mmm/dwarf/launcher/DwarfLauncherActivity.java
@@ -2,25 +2,41 @@ package mmm.dwarf.launcher;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 import com.google.android.maps.MapActivity;
 
 public class DwarfLauncherActivity extends MapActivity{
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.main);
-		
-		//Lancement de la google maps
-		Intent gmaps = new Intent(DwarfLauncherActivity.this, GoogleMapsActivity.class);
-		startActivity(gmaps);
-	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu);	
+	}
+
+	public void jouer(View MenuView) {
+
+		//Lancer le nain
+		Intent intent=new Intent(DwarfLauncherActivity.this, LaunchPlateformActivity.class);
+		startActivity(intent);
+
+	}
+
+	public void afficherMap(View MenuView) {
+		//Lancement de la google maps
+		Intent gmaps = new Intent(DwarfLauncherActivity.this, GoogleMapsActivity.class);
+		startActivity(gmaps);
+
+	}
+
 }
